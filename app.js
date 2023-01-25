2f606181f14f01fb99b7f82560239409624a67aa
UmangLadha <umangladha02@gmail.com>
2023-01-26 00:48:36 +0530

    Done with JS part

---
+++ app.js
@@ -0,0 +1,29 @@
+const inputDob = document.querySelector("#DOB")
+const inputNumb = document.querySelector("#numb")
+const checkBtn = document.querySelector("#check-btn")
+const outputTxt = document.querySelector("#output")
+
+ 
+checkBtn.addEventListener("click", checkDob)
+
+function checkDob(){
+	let dob = inputDob.value;
+	const numb = inputNumb.value;
+    calculateSum(dob, numb)
+}
+
+function calculateSum(dob,numb){
+	let sum= 0;
+	dob=dob.replaceAll("-","")
+	for(let i=0; i<dob.length; i++){
+	    let numbers = dob.charAt(i)
+		sum = sum + Number(numbers)
+	}
+	if(sum%numb===0){
+		outputTxt.innerText = "Your Birthday Is Lucky!!"
+	}
+	else{
+		outputTxt.innerText = "Your Birthday Is Not Lucky!!"
+	}
+	return sum
+}
